3c607ae3c2f353ee76d87f3909d7c50a849a05e5
alonp13 <alonp13@gmail.com>
2020-04-26 13:21:13 +0300

    empty implemntion

---
+++ real.cpp
@@ -0,0 +1,106 @@
+#include "solver.hpp"
+using namespace solver;
+
+/*
+    RealVariable Class implemention:
+*/
+
+RealVariable& RealVariable::operator + (double coe)
+{
+    return *this;
+}
+
+RealVariable& RealVariable::operator + (const RealVariable& other)
+{
+    return *this;
+}
+
+
+RealVariable& solver::operator + (double coe, const RealVariable& other) //friend
+{
+    RealVariable* tmp = new RealVariable();
+    RealVariable& ref = *tmp;
+
+    return ref;
+}
+        
+RealVariable& RealVariable::operator - (double coe) 
+{
+    return *this;
+}
+
+RealVariable& RealVariable::operator - (const RealVariable& other) 
+{
+    return *this;
+}
+
+RealVariable& solver::operator - (double coe, const RealVariable& other) //friend
+{
+    RealVariable* tmp = new RealVariable();
+    RealVariable& ref = *tmp;
+
+    return ref;
+}
+
+RealVariable& RealVariable::operator * (double coe)
+{
+    return *this;
+}
+
+RealVariable& RealVariable::operator * (const RealVariable& other)
+{
+    return *this;
+}
+
+RealVariable& solver::operator * (double coe, const RealVariable& other) //friend
+{
+    RealVariable* tmp = new RealVariable();
+    RealVariable& ref = *tmp;
+
+    return ref;
+}
+
+RealVariable& RealVariable::operator / (double coe)
+{
+    return *this;
+}
+
+RealVariable& RealVariable::operator / (const RealVariable& other)
+{
+    return *this;
+}
+
+RealVariable& solver::operator / (double coe, const RealVariable& other) //friend
+{
+    RealVariable* tmp = new RealVariable();
+    RealVariable& ref = *tmp;
+
+    return ref;
+}
+
+RealVariable& RealVariable::operator == (double coe)
+{
+    return *this;
+}
+
+RealVariable& RealVariable::operator == (const RealVariable& other)
+{
+    return *this;
+}
+RealVariable& solver::operator == (double coe, const RealVariable& other) //friend
+{
+    RealVariable* tmp = new RealVariable();
+    RealVariable& ref = *tmp;
+
+    return ref;
+}
+
+RealVariable& RealVariable::operator ^ (int pow)
+{
+    return *this;
+}
+
+double solver::solve(RealVariable& rv)
+{
+    return 0;
+}
---
+++ solver.hpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <complex>
+using namespace std;
+namespace solver 
+{
+    class RealVariable
+    {
+        private:
+
+            double m_real;
+
+        public:
+
+        RealVariable(): m_real(0){};
+        RealVariable(double real): m_real(real){};
+
+        RealVariable& operator + (double coe);
+        RealVariable& operator + (const RealVariable& other);
+        friend RealVariable& operator + (double coe, const RealVariable& other);
+        
+        RealVariable& operator - (double coe);
+        RealVariable& operator - (const RealVariable& other);
+        friend RealVariable& operator - (double coe, const RealVariable& other);
+
+        RealVariable& operator * (double coe);
+        RealVariable& operator * (const RealVariable& other);
+        friend RealVariable& operator * (double coe, RealVariable const& other);
+
+        RealVariable& operator / (double coe);
+        RealVariable& operator / (const RealVariable& other);
+        friend RealVariable& operator / (double coe, const RealVariable& other);
+
+        RealVariable& operator == (double coe);
+        RealVariable& operator == (const RealVariable& other);
+        friend RealVariable& operator == (double coe, const RealVariable& other);
+
+        RealVariable& operator ^ (int pow);
+    };
+
+    class ComplexVariable
+    {
+        private:
+
+            double m_real;
+            double m_img;
+
+        public:
+
+        ComplexVariable(): m_real(0), m_img(0){};
+        ComplexVariable(double real,double img): m_real(real),m_img(img){};
+
+
+        ComplexVariable& operator + (double coe);
+        ComplexVariable& operator + (const ComplexVariable& other);
+        ComplexVariable& operator + (std::complex<double> cmplx_coe);
+        friend ComplexVariable& operator + (std::complex<double> cmplx_coe, const ComplexVariable& other);
+        friend ComplexVariable& operator + (double coe, const ComplexVariable& other);
+        
+        ComplexVariable& operator - (double coe);
+        ComplexVariable& operator - (const ComplexVariable& other);
+        ComplexVariable& operator - (std::complex<double> cmplx_coe);
+        friend ComplexVariable& operator - (std::complex<double> cmplx_coe, const ComplexVariable& other);
+        friend ComplexVariable& operator - (double coe, const ComplexVariable& other);
+
+        ComplexVariable& operator * (double coe);
+        ComplexVariable& operator * (const ComplexVariable& other);
+        ComplexVariable& operator * (std::complex<double> cmplx_coe);
+        friend ComplexVariable& operator * (std::complex<double> cmplx_coe, const ComplexVariable& other);
+        friend ComplexVariable& operator * (double coe, const ComplexVariable& other);
+
+        ComplexVariable& operator / (double coe);
+        ComplexVariable& operator / (const ComplexVariable& other);
+        ComplexVariable& operator / (std::complex<double> cmplx_coe);
+        friend ComplexVariable& operator / (std::complex<double> cmplx_coe, const ComplexVariable& other);
+        friend ComplexVariable& operator / (double coe, const ComplexVariable& other);
+
+        ComplexVariable& operator == (double coe);
+        ComplexVariable& operator == (const ComplexVariable& other);
+        ComplexVariable& operator == (std::complex<double> cmplx_coe);
+        friend ComplexVariable& operator == (std::complex<double> cmplx_coe, const ComplexVariable& other);
+        friend ComplexVariable& operator == (double coe, const ComplexVariable& other);
+
+        ComplexVariable& operator ^ (int pow);
+    };
+
+    double solve(RealVariable& rv);
+    double solve(ComplexVariable& cv);
+};
